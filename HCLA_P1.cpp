a55c1e78a23f706548763fb4ee1e64393072af24
luis <luis.alberto.hernandez.cozatl@uabc.edu.mx>
2020-03-19 21:29:40 -0700

    LLamada Telefonica

---
+++ HCLA_P1.cpp
@@ -0,0 +1,183 @@
+
+//HCLA_P01
+//09/02/2020
+//Luis Alberto Hernandez Cozatl
+//Se realizaran 5 programas en el cual se les sacara las calificaciones,el pago total de llamadas realizadas y costos de llamadas realizadas dependiendo del lada.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#define IVA 0.08
+//****************************************************************************
+// PROTOTIPOS DE FUNCIONES
+//****************************************************************************
+void llamada(void);
+void calificaciones(void);
+void CalificacionArbol(void);
+void Agua(void);
+void Salario(void);
+
+//****************************************************************************
+// MAIN PRINCIPAL  ...  funcion principal
+//****************************************************************************
+
+int main (void)
+{
+   int op;
+
+   do{
+      system ("cls");
+      printf("************** M E N U *****************\n");
+	  printf("1.-Llamada Telefonica\n");
+	  printf("2.-Calificaciones x verdadero\n");
+	  printf("3.-Calificaciones optimizado\n");
+	  printf("4.-Agua\n");
+	  printf("5.-Salario\n");
+	  printf("6.-Salir\n");
+	  printf("\n");
+	  printf("Seleccione una opcion: \n");
+      scanf ("%d",&op);
+	  switch(op)
+	    {
+		  case 1: llamada();  break;
+		  case 2: calificaciones();  break;
+		  case 3: CalificacionArbol();  break;
+		  case 4: Agua();  break;
+		  case 5: Salario();  break;
+		}
+   }while(op != 6);
+}
+
+//**************************************************************************************
+
+
+
+void llamada(void)
+{
+	int op, op_salir, op_mint,tiemex;
+	float mint=0, subt=0, total=0, ext,iva;
+	
+    system ("cls");
+    
+	printf(" \t\t Llamada Telefonica \n\n");    
+	printf("Ingrese los minutos de la llamada realizada: ");
+	scanf("%f", &mint);
+	 
+	 if(mint >= 1)
+	  {
+	 do
+	 {	  
+	 system("cls");
+     	printf("Elija una opcion de llamada: \n");
+	    printf("1.- Llamada Local $3.00 sin limite de tiempo\n");
+	    printf("2.- Llamada Nacional $7.00 por los 3 primeros minutos y $2.00 minuto adicional\n");
+	    printf("3.- Llamada Internacional $9.00 por los 2 primeros minutos y $4.00 adicional\n");
+	    printf("4.- Salir\n");
+	    printf("Escoger Opcion: ");
+
+	       scanf("%d", &op);
+
+	 switch(op)
+	 {
+//************************** Llamada local ********************************
+	   case 1:
+	 		 system("cls");
+	 		 
+	 	if(mint >=1 )
+	 	 {
+	 	printf("\n********* Llamada sin limite de tiempo **************\n\n");
+		   subt=3.00;
+		  printf("Subtotal: $%.2f\n", subt);
+		    total=(3.00*(0.08))+3;
+		   printf("Total (IVA 8/%): $%.2f\n", total);
+		       printf("\n\nPresione cualquier tecla  para salir:  ");
+		       scanf("%d", &op_salir);  
+			 
+			 } 
+	 		 
+    		 else
+	 	    	{
+			  	printf("No tiene minutos\n");
+	 			 printf("\n\nPresione cualquier tecla  para salir: ");
+	 			  scanf("%d", &op_salir);  	
+			    }
+			 break;
+
+//*********************************** Llamada Nacional *********************************
+	   case 2:
+	   	system("cls");
+	   	
+	   	   
+	   	   	printf("\n**************** Llamada Nacional $7 + $2 min. Extra ****************\n\n");
+		        
+		          if( mint <= 3 )
+			  {  
+			     subt=7.00;
+				 iva=7*IVA;
+				 total=subt+iva;	
+			 	 printf("El Subtotal= $%.2f\n ",subt);
+			 	  printf("El iva=$%.2f \n",iva);
+			 	  printf("El total= $%.2f \n",total);
+			  }
+			else
+			 {
+			 	
+			 	 ext=mint-3;
+				 iva=(7+(ext*2))*IVA;			 	 
+			 	 subt=7+(ext*2);
+				 total=iva+subt;	
+			 	printf("El Subtotal= $%.2f\n ",subt);
+			 	  printf("El iva=$%.2f \n",iva);
+			 	  printf("El total= $%.2f \n",total);					
+			 }
+			 system("pause");
+	 	break;	
+//**************************** Llamada Internacional *********************************************** 
+	   case 3:
+	   	system("cls");
+		
+			if(mint <=2)
+			{
+				printf("\n************** Llamada InterNacional $9 + $4 min. Extra *************** \n\n");
+				subt=9.00;
+				iva=9.00*IVA;
+				total=subt+iva;
+				printf("El Subtotal= $%.2f\n ",subt);
+			 	printf("El iva=$%.2f \n",iva);
+			 	printf("El total= $%.2f \n",total);
+			}
+			else
+			{
+				printf("\n************** Llamada InterNacional $9 + $4 min. Extra *******************\n\n");
+				ext=mint-2;
+				iva=(9.00+(ext*4))*IVA;
+				subt=9.00+(ext*4);
+				total=iva+subt;
+				printf("El Subtotal= $%.2f\n ",subt);
+			 	printf("El iva=$%.2f \n",iva);
+			 	printf("El total= $%.2f \n",total);
+			}
+			system("pause");
+			break;
+		 
+	   }//cierre del switch
+        }while( ( op_salir != 4) && (op !=4) );// cierre del do
+  
+	  } 
+}
+//**************************************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
